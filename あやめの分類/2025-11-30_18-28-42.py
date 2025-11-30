0f0caee1410d1a8329daf77f478b315ae587e678
RyoyaKurihara <t.4178.nxivn47@gmail.com>
2025-11-30 22:01:51 +0900

    あやめの分類

--- あやめの分類/2025-11-30_18-28-42.py
+++ あやめの分類/2025-11-30_18-28-42.py
@@ -1,7 +1,14 @@
 
 import pandas as pd
+from sklearn import tree
 
 df = pd.read_csv('./iris.csv')
-df.isnull().any(axis = 0)
+df2 = df.dropna(how = 'any', axis = 0)
 
+x = df2[['がく片長さ', 'がく片幅', '花弁長さ', '花弁幅']]
+t = df2['種類']
+
+model = tree.DecisionTreeClassifier(max_depth = 2, random_state=0)
+model.fit(x, t)
+model.score(x, t)
 
