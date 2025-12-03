f0971da4cf97a47d54c08bb456e215da0700d0be
RyoyaKurihara <t.4178.nxivn47@gmail.com>
2025-12-03 22:51:24 +0900

    映画の興行収入

--- あやめの分類/2025-12-01_05-14-21.py
+++ あやめの分類/2025-12-01_05-14-21.py
@@ -1,3 +1,19 @@
 
+import pandas as pd
+from sklearn import tree
+from sklearn.model_selection import train_test_split
 
+df = pd.read_csv('./iris.csv')
+df2 = df.dropna(how = 'any', axis = 0)
 
+x = df2[['がく片長さ', 'がく片幅', '花弁長さ', '花弁幅']]
+t = df2['種類']
+
+x_train, x_test, y_train, y_test = train_test_split(x, t, test_size = 0.3, random_state = 0)
+
+# print(x_train.shape)
+# print(x_test.shape) 
+
+model = tree.DecisionTreeClassifier(max_depth = 2, random_state=0)
+model.fit(x_train, y_train)
+model.score(x_test, y_test)
